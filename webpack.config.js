438ca585e3bc43a94e0f81ea9400333d3a682d68
kieron <kieron.wray@gmail.com>
2019-03-30 19:24:51 +0000

    added webpack and config

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  entry: __dirname + '/app/app.tsx',
+  output: {
+    path: __dirname + '/app',
+    filename: 'app.bundle.js'
+  },
+  module: {
+    rules: [{ test: /\.(ts|tsx)$/, loader: 'awesome-typescript-loader' }]
+  }
+}
